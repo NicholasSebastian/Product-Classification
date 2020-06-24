d1db7cfced5358eac23fc309fd3ee4a991f3e6cd
AetherCrusade <nicholashendrata110900@gmail.com>
2020-06-24 23:00:03 +0800

    split data into training and test data

--- ConvNeuralNetwork.py
+++ ConvNeuralNetwork.py
@@ -5,28 +5,26 @@ import numpy as np
 import pickle
 
 # Read the preprocessed images data.
-image_pickle = open("training_images.pickle", "rb")
-images = pickle.load(image_pickle)
-image_pickle.close()
-
-label_pickle = open("training_labels.pickle", "rb")
-labels = pickle.load(label_pickle)
-label_pickle.close()
+dataset_pickle = open("dataset.pickle", "rb")
+images, labels, test_images, test_labels = pickle.load(dataset_pickle)
+dataset_pickle.close()
 
 # Convert to numpy arrays and scale.
 images = np.array(images / 255.0)
 labels = np.array(labels)
+test_images = np.array(test_images / 255.0)
+test_labels = np.array(test_labels)
 
 # Create the sequential model.
 model = Sequential()
 
 # Layer 1
-model.add(Conv2D(64, (3, 3), input_shape=images.shape[1:]))
+model.add(Conv2D(64, (5, 5), input_shape=images.shape[1:]))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Layer 2
-model.add(Conv2D(64, (3, 3), input_shape=images.shape[1:]))
+model.add(Conv2D(64, (5, 5), input_shape=images.shape[1:]))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -39,9 +37,13 @@ model.add(Dense(42))
 model.add(Activation("sigmoid"))
 
 # Compile model.
-model.compile(loss="sparse_categorical_crossentropy",
+model.compile(loss="categorical_crossentropy",
               optimizer="adam",
               metrics=["accuracy"])
 
 # Train model.
-model.fit(images, labels, batch_size=32, epochs=3, validation_split=0.1)
+model.fit(images, labels, batch_size=128, epochs=3, validation_split=0.1)
+
+# Evaluate model.
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
+print(f"Test Accuracy: {test_acc}\nTest Loss: {test_loss}")
--- LoadAndPreprocessData.py
+++ LoadAndPreprocessData.py
@@ -35,14 +35,20 @@ for image_array, category in training_data:
     images.append(image_array)
     labels.append(category)
 
-# Convert into numpy arrays and reshape.
+# Split the arrays into 9:1, training data and testing data respectively.
+test_images = []
+test_labels = []
+ratio = int(len(images) * 0.1)
+for _ in range(ratio):
+    test_images.append(images.pop())
+    test_labels.append(labels.pop())
+
+# Reshape image arrays.
 images = np.array(images).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+test_images = np.array(images).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 # Store all the preprocessed data onto a file.
-image_pickle = open("training_images.pickle", "wb")
-pickle.dump(images, image_pickle)
-image_pickle.close()
-
-label_pickle = open("training_labels.pickle", "wb")
-pickle.dump(labels, label_pickle)
-label_pickle.close()
+dataset = (images, labels, test_images, test_labels)
+dataset_pickle = open("dataset.pickle", "wb")
+pickle.dump(dataset, dataset_pickle)
+dataset_pickle.close()
